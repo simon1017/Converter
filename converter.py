266007696864abdbd9218e91d38f1816ac8b3a01
Simon Ekman <simon.ekman1017@gmail.com>
2023-05-19 20:09:25 +0200

    first commit

--- converter.py
+++ converter.py
@@ -1,1 +1,16 @@
 # converts pdf to mp3 file.
+import pyttsx3,PyPDF2
+
+pdfreader = PyPDF2.PdfReader(open('test.pdf','rb'))
+speaker = pyttsx3.init()
+
+
+for page_num in range(pdfreader.numPages):
+    text =+ pdfreader.getPage(page_num).extract_text()
+
+clean_text = text.strip().replace('\n',' ')
+print(clean_text)
+
+speaker.save_to_file(clean_text, 'story.mp3')
+speaker.runAndWait()
+speaker.stop()

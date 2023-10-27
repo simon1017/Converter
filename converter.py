786fa5ec5027354c727ec3735db014fe4dee4ef4
Simon Ekman <simon.ekman1017@gmail.com>
2023-10-27 17:32:57 +0200

    test

--- converter.py
+++ converter.py
@@ -6,7 +6,7 @@ speaker = pyttsx3.init()
 
 
 for page_num in range(pdfreader.numPages):
-    text =+ pdfreader.getPage(page_num).extract_text()
+    text += pdfreader.getPage(page_num).extract_text()
 
 clean_text = text.strip().replace('\n',' ')
 print(clean_text)

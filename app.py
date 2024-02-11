d2b68251af376bba8b01fa52ff0883863a18ec15
Simon Ekman <simon.ekman1017@gmail.com>
2024-02-11 03:03:16 +0100

    converter now objectoriented

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import converter
 
 class PDFToWavConverter(BoxLayout):
     pdf_path_input = ObjectProperty(None)
-    #save_path_input = ObjectProperty(None)
     wav_name_input = ObjectProperty(None)
     page_start_input = ObjectProperty(None)
     page_end_input = ObjectProperty(None)
@@ -20,67 +19,17 @@ class PDFToWavConverter(BoxLayout):
         self.spacing = 10
         self.padding = 10
 
-        # Label for PDF file path
-        #pdf_label = Label(text='PDF File Path:')
-        #self.add_widget(pdf_label)
-
-        # Button to select PDF file
-        #pdf_button = Button(text='Select PDF', on_press=self.select_pdf_path)
-        #self.add_widget(pdf_button)
-
-        # Text input for PDF file path
-        #self.pdf_path_input = TextInput(hint_text='Enter PDF file path')
-        #self.add_widget(self.pdf_path_input)
-
-        # Label for WAV file name
-        #wav_label = Label(text='WAV File Name:')
-        #self.add_widget(wav_label)
-
-        # Text input for WAV file name
-        #self.wav_name_input = TextInput(hint_text='Enter WAV file name')
-        #self.add_widget(self.wav_name_input)
-        # Text input for start page
-        #self.page_start_input = TextInput(hint_text='Enter start page')
-        #self.add_widget(self.page_start_input)
-        # Text input for end page
-        #self.page_end_input = TextInput(hint_text='Enter end page')
-        #self.add_widget(self.page_end_input)
-
-        # Button to select destination folder
-        #save_button = Button(text='Select Save Path', on_press=self.select_save_path)
-        #self.add_widget(save_button)
-
-        # Button to convert PDF to WAV
-        #convert_button = Button(text='Convert PDF to WAV', on_press=self.convert_pdf_to_wav)
-        #self.add_widget(convert_button)
-
     def select_pdf_path(self):
         self.popup = Popup(title='Select PDF File', content=FileChooserIconView(), size_hint=(0.9, 0.9))
         self.popup.content.bind(on_submit=self.on_pdf_select)
         self.popup.open()
 
-    #def select_save_path(self):
-        #root = tk.Tk()
-        #root.withdraw()  # Hide the main tkinter window
-        #save_path = filedialog.askdirectory()  # Open the directory picker dialog
-        #self.save_path_input.text = save_path
-        #print("Selected directory:", save_path)
-    #def select_save_path(self):
-    #    self.popup = Popup(title='Select Destination Folder', content=FileChooserIconView(path=os.getcwd(), dirselect=True), size_hint=(0.9, 0.9))
-    #    self.popup.content.bind(on_submit=self.on_save_select)
-    #    self.popup.open()
-
     def on_pdf_select(self, instance, path, touch):
         self.pdf_path_input.text = path[0]
         self.popup.dismiss()
 
-    #def on_save_select(self, instance, path, touch):
-    #    self.save_path_input.text = path[0]
-    #    self.popup.dismiss()
-
     def convert_pdf_to_wav(self):
         pdf_path = self.pdf_path_input.text.strip()
-        #save_path = self.save_path_input.text.strip()
         wav_name = self.wav_name_input.text.strip()
         page_start = int(self.page_start_input.text.strip())
         page_end = int(self.page_end_input.text.strip())
@@ -97,7 +46,7 @@ class PDFToWavConverter(BoxLayout):
             self.show_popup("Error", "Destination folder is required.")
             return
 
-        converter.pdf_to_wav(pdf_path, page_start, page_end, wav_name)
+        converter.Converter(pdf_path, page_start, page_end, wav_name).convert()
 
         self.show_popup("Success", f"Conversion completed. WAV file saved as {wav_name}")
 
--- converter.py
+++ converter.py
@@ -2,63 +2,51 @@ import pyttsx3
 import PyPDF2
 import os
 
-def text_filter(text):
-    # split text into lines
-    lines = text.split('\n')
-    # remove last line
-    new_text = ""
-    for line in range(0, len(lines)-1):
-        new_text += lines[line]
-    return new_text
+class Converter:
+    def __init__(self, pdf_path, page_start, page_end, wav_name):
+        self.pdf_path = pdf_path
+        self.page_start = page_start
+        self.page_end = page_end
+        self.wav_name = wav_name
 
-def clean_text(text):
-    #text = text_filter(text)
-    text = text.strip().replace('\n', ' ')
-    return text
+    def convert(self):
+        pdf_reader = PyPDF2.PdfReader(open(self.pdf_path, 'rb'))
+        speaker = pyttsx3.init()
 
-def get_os_path(file_name):
-    return os.path.join(os.getcwd(), file_name)
+        # Choose voice
+        voice_model = "com.apple.speech.synthesis.voice.daniel.premium"
+        speaker.setProperty('voice', voice_model)
 
-def pdf_to_wav(pdf_path, page_start, page_end, wav_name):
-    # init
-    pdf_reader = PyPDF2.PdfReader(open(pdf_path, 'rb'))
-    speaker = pyttsx3.init()
+        # Adjust speed
+        rate = speaker.getProperty('rate')
+        speaker.setProperty('rate', rate - 30)
 
-    # chose voice
-    voice_model = "com.apple.speech.synthesis.voice.daniel.premium"
-    speaker.setProperty('voice', voice_model)
+        text = []
+        new_text = []
 
-    # chose speed
-    rate = speaker.getProperty('rate')
-    speaker.setProperty('rate', rate - 30)
-    text = []
-    new_text = []
+        for page_num in range(self.page_start - 1, self.page_end):
+            print(f"cleaning text page {page_num + 1}...")
+            text.append(pdf_reader.pages[page_num].extract_text())
+            new_text.append(self.clean_text(text[-1]))
 
-    for page_num in range(page_start - 1, page_end):  # Adjusted the range to include the specified pages
-        print(f"cleaning text page {page_num + 1}...")
-        text.append(pdf_reader.pages[page_num].extract_text())
-        new_text.append(clean_text(text[-1]))
+            print("//---------------//")
+            print(new_text[-1])
+            print("//---------------//")
 
-        print("//---------------//")
-        print(new_text[-1])
-        print("//---------------//")
+        save_path = os.path.join(os.getcwd(), "audio_files")
+        print(save_path)
+        full_text = ""
+        for page in new_text:
+            full_text += str(page)
+        speaker.save_to_file(full_text, os.path.join(save_path, self.wav_name))
+        speaker.runAndWait()
+        speaker.stop()
+        print(f"saved as {self.wav_name}")
 
-    # get path and save wav file
-    #full_path = get_os_path(wav_name)
-    #print(full_path)
-    save_path = os.path.join(os.getcwd(), "audio_files")
-    print(save_path)
-    full_text = ""
-    for page in new_text:
-        full_text += str(page)
-    speaker.save_to_file(full_text, os.path.join(save_path, wav_name))
-    speaker.runAndWait()
-    speaker.stop()
-    print(f"saved as {wav_name}")
+    @staticmethod
+    def clean_text(text):
+        text = text.strip().replace('\n', ' ')
+        return text
 
-#wav_name = "test_full.wav"
-#pdf_path = 'test.pdf'
-#page_start = 1
-#page_end = 2
-
-#pdf_to_wav(pdf_path, page_start, page_end, wav_name)
+if __name__ == "__main__":
+    print("Wrong file, run app.py")
